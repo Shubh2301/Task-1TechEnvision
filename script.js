d32e30a68f23a31e0617b7398b241020ee6da09a
Shubham Kamble <shubhamkamble3011@gmail.com>
2026-03-12 17:54:57 +0530

    gsap-animation

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+gsap.from(".navbar", {
+  y: -200,
+  duration: 1,
+  opacity:0,
+})
+
+gsap.from('.left-text',{
+  x:-300,
+  opacity:0,
+  delay:1
+})
+
+gsap.from('.model-img',{
+    x:150,
+    delay:1.3,
+    opacity:0
+})
+
+gsap.from('.btn',{
+    x:150,
+    delay:1.3,
+    opacity:0
+})
